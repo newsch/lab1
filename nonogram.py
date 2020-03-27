69a1845ad06e41160b1d02e3712ddcc18325b1e0
Manu Patil <mpatil99@gmail.com>
2020-03-27 02:50:19 -0400

    Generates possibilities working

--- nonogram.py
+++ nonogram.py
@@ -4,7 +4,7 @@ from typing import List, Optional, Iterator, Dict, Tuple
 from itertools import zip_longest
 
 import pycosat
-
+import copy
 
 # problem is list of list of runs
 # 0: row runs
@@ -21,7 +21,7 @@ CNF = List[Clause]
 
 def parse_alpha_encoding(src: str) -> Problem:
     """Parse a string of an alpha-encoded nonogram.
-    
+
     The format is explained more at <https://rosettacode.org/wiki/Nonogram_solver>.
     """
     return [
@@ -57,9 +57,46 @@ def read_file(path: str) -> Iterator[Problem]:
     for encoded_problem in txt.split('\n\n'):
         yield parse_alpha_encoding(encoded_problem)
 
+def create_possibilities(runs, length):
+
+    def _create_possibilities(runs, mark, min_start, possibility):
+        #Base Case: add current possibility and return
+        if len(runs) == 0:
+            possibilities.append(possibility)
+            return
+        elif min_start >= len(possibility):
+            return
+        for start in range(0, len(possibility) - runs[0] +1 ):
+            #Make a copy of the array for every position that the run can start in
+            p_copy = copy.deepcopy(possibility)
+            for i in range(start, start+runs[0]):
+                if(i < len(possibility)):
+                    p_copy[i] = mark
+                else:
+                   #If I somehow exceeds
+                   print("How did you get here")
+                   return
+            _create_possibilities(runs[1:], mark, start + runs[0]+1, p_copy)
+    empty_possibility = [0 for i in range(length)]
+    possibilities = []
+    _create_possibilities(runs, 1, 0, empty_possibility)
+
+    return possibilities
 
 def convert_to_sat(problem: Problem) -> Tuple[CNF, Dict[Tuple[int, int], int]]:
+    rows = problem[0]
+    cols = problem[1]
+    row_possibilities = []
+    col_possibilities = []
+    #handle row
+    for row in rows:
+        row_possibilities.append(create_possibilities(row, len(cols)))
+    #handle col
+    for col in cols:
+        col_possibilities.append(create_possibilities(col, len(rows)))
 
+    print("Row: ", row_possibilities)
+    print("Col: ", col_possibilities)
     # TODO: return form and mapping to grid locations
     pass
 
@@ -72,7 +109,7 @@ def solve(problem: Problem) -> Optional[Solution]:
     res = pycosat.solve(clauses)
     if isinstance(res, str):
         return None
-    
+
     solution = []
     for y in range(height):
         row = []
@@ -82,7 +119,7 @@ def solve(problem: Problem) -> Optional[Solution]:
             pass
             row.append(value)
         solution.append(row)
-    
+
     return solution
 
 
@@ -92,6 +129,8 @@ def main():
     # TODO: solve nonograms here
     for nonogram in read_file(file):
         # print(nonogram)
+        print(nonogram)
+        convert_to_sat(nonogram)
         print_nonogram(nonogram)
 
 
